af8ada29c1c8fb308d651c345b63c84587775d2e
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-05-15 21:56:55 +0630

    Initial commit

---
+++ basic/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var x float64
+	fmt.Println("Hello, Ashmoon!")
+	fmt.Println("The square root of", x, "is", math.Sqrt(float64(x)))
+}
