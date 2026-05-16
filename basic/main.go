0154410d5231b350cccedc139a1e9ecf54da549b
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-05-16 22:24:37 +0630

    -

--- basic/main.go
+++ basic/main.go
@@ -9,4 +9,7 @@ func main() {
 	var x float64
 	fmt.Println("Hello, Ashmoon!")
 	fmt.Println("The square root of", x, "is", math.Sqrt(float64(x)))
+	fmt.Println("The value of Pi is", math.Pi)
+	fmt.Println("The value of e is", math.E)
+	fmt.Println(2 + 2*3 - 4/2)
 }
